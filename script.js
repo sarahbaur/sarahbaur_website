f985a96eb781396a95edea3a2a140a379b867be9
Sarah Baur <133791368+sarahbaur@users.noreply.github.com>
2024-07-21 16:21:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+// Get the modal image container
 var modalImg = document.getElementById("img01");
 
+// Add click event listeners to each image in the gallery
 document.querySelectorAll('.gallery-img').forEach(img => {
-    img.onclick = function(){
+    img.onclick = function() {
         modal.style.display = "block";
-        modalImg.src = this.src;
+        modalImg.src = this.src; // Set the src of the modal image to the src of the clicked image
+        modalImg.alt = this.alt; // Set the alt text of the modal image to the alt text of the clicked image
     }
 });
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), close the modal and clear the image source
 span.onclick = function() {
     modal.style.display = "none";
+    modalImg.src = ''; // Clear the image source to ensure it doesn't flash the old image upon next modal open
 }
